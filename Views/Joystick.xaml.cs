9986e52f1bbe85a551e4b9089815207f4017cb0b
yankovs <shibushka@gmail.com>
2020-03-15 17:49:39 +0200

    Fixed joystick XML/code-behind, added centerKnob method to joystick

---
+++ Views/Joystick.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF_Project.Views
+{
+    public partial class Joystick : UserControl
+    {
+       public Joystick()
+        {
+            InitializeComponent();
+        }
+
+        private void centerKnob_Completed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
