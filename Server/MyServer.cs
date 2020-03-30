3e9cf443308f4c5a17cb98ae7bddf2abff3f7f83
LIAD AVRAHAM ELINSON <liadavraham.elinson@live.biu.ac.il>
2020-03-30 23:46:24 +0300

    dealing with timeout v1

--- Server/MyServer.cs
+++ Server/MyServer.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WPF_Project.Server
@@ -25,10 +26,14 @@ namespace WPF_Project.Server
                 try
                 {
                     this.client = new TcpClient();
-                    client.Connect(IPAddress.Parse(ip), port);
+                    this.client.SendTimeout = 10000;
+                    this.client.ReceiveTimeout = 10000;
+                    this.client.Connect(ip, port);
                     this.ns = client.GetStream();
                     this.sw = new StreamWriter(ns);
+                    this.sw.BaseStream.WriteTimeout = 10000;
                     this.sr = new StreamReader(ns);
+                    this.sr.BaseStream.ReadTimeout = 10000;
                     connected = true;
                 }
                 catch (SocketException)
@@ -48,9 +53,19 @@ namespace WPF_Project.Server
                     this.sw.WriteLine(command);
                     this.sw.Flush();
                 }
-                catch
+                catch (Exception e)
                 {
-                    throw new Exception("Unable to write");
+                    this.sw.Flush();
+                    if (e.Message != "Unable to read data from the transport connection: A connection attempt failed" +
+                        "because the connected party did not properly respond after a period of time, or established" +
+                        " connection failed because connected host has failed to respond.")
+                    {
+                        throw new Exception("Unable to write");
+                    }
+                    else
+                    {
+                        throw new Exception("Timeout (writing)");
+                    }
                 }
             }
             else
@@ -65,13 +80,23 @@ namespace WPF_Project.Server
             {
                 try
                 {
-                    string line = sr.ReadLine();                    
-                    Console.WriteLine(line);                    
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
                     return line;
                 }
-                catch
+                catch (Exception e)
                 {
-                    throw new Exception("Unable to read");
+                    sr.DiscardBufferedData();
+                    if (e.Message != "Unable to read data from the transport connection: A connection attempt failed" +
+                        "because the connected party did not properly respond after a period of time, or established" +
+                        " connection failed because connected host has failed to respond.")
+                    {
+                        throw new Exception("Unable to read");
+                    }
+                    else
+                    {
+                        throw new Exception("Timeout (reading)");
+                    }
                 }
             }
             else
@@ -84,9 +109,9 @@ namespace WPF_Project.Server
         {
             if (isConnected())
             {
-                ns.Close();
                 sw.Close();
                 sr.Close();
+                ns.Close();
                 client.Close();
                 connected = false;
             }
